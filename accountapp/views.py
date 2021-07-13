f1d3f08df7e5d9c267891034dde0cc3b257433a4
namvandy <namvandy1205@naver.com>
2021-07-13 17:03:34 +0900

    7/13 1st commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -26,3 +29,19 @@ def hello_world(request):
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html',
                       context={'hello_world_list': hello_world_list})
+
+
+
+
+
+
+
+#     Class Based View
+class AccountCreateView(CreateView):
+    model= User         #무엇을 만들지 #django에서 지원하는 기능 User
+    form_class = UserCreationForm #django에서 지원하는 기능 UserCreationForm
+    #두개가 존재, 비밀번호 입력창, 출력창
+    success_url = reverse_lazy('accountapp:hello_world')
+    #reverse를 바로쓰면안됨. _lazy를 쓰는이유는 실체 객체생성후 필요할 때 호출해야함. function base와 class base의 차이
+    template_name = 'acoountapp/create.html'
+    #이 4개로 회원가입 로직 완성
