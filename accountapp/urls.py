a48e8af85c6357a0f8d27f15248292e33152c1d3
namvandy <namvandy1205@naver.com>
2021-07-13 18:12:52 +0900

    7/13 3rd commit

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from accountapp.views import hello_world, AccountCreateView
 
@@ -5,5 +6,14 @@ app_name = 'accountapp'
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
 
-    path('create/', AccountCreateView.as_view(), name='create')
+    path('create/', AccountCreateView.as_view(), name='create'), #회원가입
+
+    path('login/',
+         LoginView.as_view(template_name='accountapp/login.html'),
+         name='login'),
+    #로그인
+
+    path('logout/',
+         LogoutView.as_view(),
+         name='logout')
 ]
