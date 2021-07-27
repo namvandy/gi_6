f7d84cb3a11f4424293ff94e7ba90bf67351fd3d
namv <a01058303550@gmail.com>
2021-07-27 18:04:37 +0900

    7/27 4th commit
    views.py change, update
    
    프로필 유무에 따른 if문 생성
    프로필 폰트 변경
    create <update.html>
    profile update permission

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from profileapp.views import ProfileCreateView
+from profileapp.views import ProfileCreateView, ProfileUpdateView
 
 app_name = 'profileapp'
 
 urlpatterns=[
     path('create/', ProfileCreateView.as_view(), name='create'),
+    #수정하는 것이므로 key를 넘겨받아야함
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name='update'),
 
 ]
