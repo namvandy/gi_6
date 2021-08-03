8c63d25b1d4782d5d3908443801480701909f25c
namv <a01058303550@gmail.com>
2021-08-03 17:52:09 +0900

    8/03 3rd commit articleapp/ Updateview

--- articleapp/views.py
+++ articleapp/views.py
@@ -4,11 +4,11 @@ from django.shortcuts import render
 
 #CRUD모두 사용(게시판)
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
-
+from django.urls import reverse
 
 class ArticleCreateView(CreateView):
     model = Article
@@ -25,3 +25,14 @@ class ArticleDetailView(DetailView):
     model = Article
     context_object_name = 'target_article'
     template_name = 'articleapp/detail.html'
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    form_class = ArticleCreationForm
+    context_object_name = 'target_article'
+    template_name = 'articleapp/update.html'
+    # success_url = reverse_lazy('articleapp:list')
+    #동적으로 url지정
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk':self.object.pk})
