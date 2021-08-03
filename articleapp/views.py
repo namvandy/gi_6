159ed2ef8f499df46b33b7826cbff604088c758d
namv <a01058303550@gmail.com>
2021-08-03 16:50:37 +0900

    8/03 1st commit createview

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#CRUD모두 사용(게시판)
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from articleapp.forms import ArticleCreationForm
+from articleapp.models import Article
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    form_class = ArticleCreationForm
+    success_url = reverse_lazy('articleapp:list')
+    template_name = 'articleapp/create.html'
+
+    def form_valid(self, form):
+        form.instance.writer = self.request.user #request를 보내는 user로 writer를 할당
+        return super().form_valid(form)
