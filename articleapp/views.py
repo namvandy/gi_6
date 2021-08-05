e642c77e37fa1491cfdb06989c3a3b48cea76650
namv <a01058303550@gmail.com>
2021-08-05 11:13:35 +0900

    8/05 1st commit decorator

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,24 +1,32 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 
 #CRUD모두 사용(게시판)
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
 from django.urls import reverse
 
+@method_decorator(login_required,'get')
+@method_decorator(login_required,'post')
 class ArticleCreateView(CreateView):
     model = Article
     form_class = ArticleCreationForm
-    success_url = reverse_lazy('articleapp:list')
+    # success_url = reverse_lazy('articleapp:list')
     template_name = 'articleapp/create.html'
 
     def form_valid(self, form):
         form.instance.writer = self.request.user #request를 보내는 user로 writer를 할당
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('articleapp:list', kwargs={'pk': self.object.pk})
 
 
 class ArticleDetailView(DetailView):
@@ -26,7 +34,8 @@ class ArticleDetailView(DetailView):
     context_object_name = 'target_article'
     template_name = 'articleapp/detail.html'
 
-
+@method_decorator(article_ownership_required,'get')
+@method_decorator(article_ownership_required,'post')
 class ArticleUpdateView(UpdateView):
     model = Article
     form_class = ArticleCreationForm
@@ -37,6 +46,8 @@ class ArticleUpdateView(UpdateView):
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk':self.object.pk})
 
+@method_decorator(article_ownership_required,'get')
+@method_decorator(article_ownership_required,'post')
 class ArticleDeleteView(DeleteView):
     model = Article
     context_object_name = 'target_article'
