a1cbf82575e8d9fa9cbd4e31afaf2aa612419650
namv <a01058303550@gmail.com>
2021-08-05 11:50:26 +0900

    8/05 2nd commit decorator

--- articleapp/views.py
+++ articleapp/views.py
@@ -26,7 +26,7 @@ class ArticleCreateView(CreateView):
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse('articleapp:list', kwargs={'pk': self.object.pk})
+        return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
 
 
 class ArticleDetailView(DetailView):
