549b06ed174acb42148bd6abb3eeab724008bf3d
namv <a01058303550@gmail.com>
2021-08-05 12:13:12 +0900

    8/05 3rd commit comment function

--- articleapp/views.py
+++ articleapp/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 #CRUD모두 사용(게시판)
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 
 from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
@@ -53,3 +53,10 @@ class ArticleDeleteView(DeleteView):
     context_object_name = 'target_article'
     success_url = reverse_lazy('articleapp:list')
     template_name = 'articleapp/delete.html'
+
+
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'articleapp/list.html'
+    paginate_by = 20 #pagination 20개 게시글
