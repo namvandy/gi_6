97fe3568ebc03dd6c5ecdc8dffe5e0ba5ee25b2a
namv <a01058303550@gmail.com>
2021-08-09 12:38:42 +0900

    8/09 5th commit
    delete.html

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
@@ -19,3 +19,12 @@ class CommentCreateView(CreateView):
         form.instance.writer = self.request.user
         form.instance.article_id = self.request.POST.get('article_pk')
         return super().form_valid(form)
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+    form_class = CommentCreationForm
+    template_name = 'commentapp/delete.html'
+
+    def get_success_url(self):
+
+        return reverse('articleapp:detail',kwargs={'pk':self.object.article.pk})
