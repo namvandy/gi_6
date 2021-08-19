37ef0804ea4ddc9e34e75099be0f65130647d04b
namv <a01058303550@gmail.com>
2021-08-19 11:39:58 +0900

    8/19 2nd commit
    templates에서 모두 보여주는게 아닌 view에서 mixin 후 필터링해서 보여주는 식으로 변경

--- projectapp/views.py
+++ projectapp/views.py
@@ -6,7 +6,9 @@ from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic.list import MultipleObjectMixin
 
+from articleapp.models import Article
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
 
@@ -22,13 +24,19 @@ class ProjectCreateView(CreateView):
         return reverse('projectapp:detail', kwargs={'pk': self.object.pk})
 
 
-class ProjectDetailView(DetailView):
+class ProjectDetailView(DetailView,MultipleObjectMixin):
     model = Project
     context_object_name = 'target_project'
     template_name = 'projectapp/detail.html'
+
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        article_list = Article.objects.filter(project=self.object) #self.object = target_project
+        return super().get_context_data(object_list=article_list, **kwargs)
 
 class ProjectListView(ListView):
     model = Project
     context_object_name = 'project_list'
     template_name = 'projectapp/list.html'
-    paginate_by = 10
+    paginate_by = 20
