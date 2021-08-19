4de864469fb6360daa23a1229e3bd8ee5178d737
namv <a01058303550@gmail.com>
2021-08-19 12:37:15 +0900

    8/19 5st commit
    subscribe/models.py
    class Subscription.
    user와 project를 묶어 class Meat: unique_together
    =>하나의 사용자는 한 번의 구독만 가능하게 하나로 묶어버림.

--- subscribeapp/models.py
+++ subscribeapp/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
-
+from projectapp.models import Project
 # Create your models here.
+# on_delete = CASCADE 유저가 없어진다면 구독정보도 다 사라지게
+
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='subscription', null=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='subscription', null=False)
+
+    class Meta:
+        unique_together = ['user','project']    # 하나의 사용자는 하나의 구독만 가능하게 옵션. 두 개를 묶어 하나로 만듬.
