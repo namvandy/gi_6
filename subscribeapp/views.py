3e3941bad10030477572b406b69f81bd974f2dab
namv <a01058303550@gmail.com>
2021-08-23 12:30:38 +0900

    8/23 3rd commit 구독한 리스트를 보는 기능 추가, 페이지
    1.find user subscripted projects
    2.find articles in projects
    In SQL WHERE -> project__in
    Django: field lookups

--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.utils.decorators import method_decorator
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView
 from django.urls import reverse
+
+from articleapp.models import Article
 from projectapp.models import Project
 from subscribeapp.models import Subscription
 
@@ -26,3 +28,16 @@ class SubscriptionView(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
 
         return reverse('projectapp:detail',kwargs={'pk': kwargs['project_pk']}) #구독게시판으로 넘어가게
+
+@method_decorator(login_required, 'get')
+class SubscriptionListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'subscribeapp/list.html'
+    paginate_by = 20
+
+    def get_queryset(self):
+        project_list = Subscription.objects.filter(user=self.request.user).values_list('project')    #Subscription에 project와 db를 연결해놓았음 / 유저가 보유하고 있는 구독한 project만 리스트화해 담음.
+        article_list = Article.objects.filter(project__in=project_list)
+        return article_list
+
