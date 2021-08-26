b54b49fa42e6d3c678d0e3f6c8d3bcb46f87f30b
namv <a01058303550@gmail.com>
2021-08-26 12:07:00 +0900

    8/26 2nd commit 게시글작성시 글자에 블럭을 주어 크기조정이나 글꼴변환 등의 기능추가 <WYSIWYG=What You See Is What You Get> https://yabwe.github.io/medium-editor/ https://github.com/yabwe/medium-editor Via an external CDN거를 복사->articleapp-create.html 상단에 복붙 Usage- <script>var editor = new MediumEditor('.editable');</script> ->articleapp-create.html 하단에 복붙 .editable이라는 클래스를 작성창에 지정해주어야함. ->articleapp-forms.py 의 content를 커스터마이징해야함. ================= articleapp-deatil.html {{target_article.content | safe }} -> | safe 필터를 넣어 올바르게 출력되게

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,15 @@
+from django import forms
 from django.forms import ModelForm
 
 from articleapp.models import Article
 
 
 class ArticleCreationForm(ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'class':'editable',
+                                                           'style':'min-height: 10rem;'
+                                                                    'text-align:left;'}))
+
+    #content=문자열을 입력받을 수 있는 형태, 클래스는 editable / attrs로 미리 html을 지정
     class Meta:
         model = Article
         fields = ['title','image','project' ,'content'] #클라이언트로부터 입력받을 것들
